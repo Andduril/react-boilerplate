d5cf894ab216cf96380bbb66eda631900d61989c
Anddy Labrut <73158252+Andduril@users.noreply.github.com>
2025-01-19 15:29:45 +0100

    feat: enhance LoginForm with confirm password validation and error handling

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,13 +1,19 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
 import { Button } from '../ui/button';
 
-const formSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(8),
-});
+const formSchema = z
+  .object({
+    email: z.string().email({ message: 'Invalid email adress' }),
+    password: z.string().min(8, { message: 'Password must be at least 8 characters long' }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: 'Passwords do not match',
+  });
 
 const LoginForm = () => {
   const form = useForm<z.infer<typeof formSchema>>({
@@ -17,6 +23,7 @@ const LoginForm = () => {
       password: '',
     },
   });
+  const { handleSubmit, clearErrors } = form;
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
     console.log(values);
@@ -24,7 +31,7 @@ const LoginForm = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <FormField
           control={form.control}
           name="email"
@@ -32,9 +39,9 @@ const LoginForm = () => {
             <FormItem>
               <FormLabel>Email</FormLabel>
               <FormControl>
-                <input placeholder="shadcn@gigachad.com" {...field} />
+                <input placeholder="shadcn@gigachad.com" {...field} onFocus={() => clearErrors('email')} />
               </FormControl>
-              <FormDescription>This is the email you used to sign up.</FormDescription>
+              {/* <FormDescription>This is the email you used to sign up.</FormDescription> */}
               <FormMessage />
             </FormItem>
           )}
@@ -46,9 +53,22 @@ const LoginForm = () => {
             <FormItem>
               <FormLabel>Password</FormLabel>
               <FormControl>
-                <input type="password" {...field} />
+                <input type="password" {...field} onFocus={() => clearErrors('password')} />
               </FormControl>
-              <FormDescription>Must be at least 8 characters long.</FormDescription>
+              {/* <FormDescription>Must be at least 8 characters long.</FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl>
+                <input type="password" {...field} onFocus={() => clearErrors('confirmPassword')} />
+              </FormControl>
               <FormMessage />
             </FormItem>
           )}
